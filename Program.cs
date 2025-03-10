f9ed4a9fd86d3f0c4da4e413ee0f46373dc14f07
Jan Leirvaag <lja022025@gmail.com>
2025-03-10 14:34:02 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+//var salesTotalDir = Path.Combine(Directory.GetCurrentDirectory(), framework.assetsPath());
+
+Directory.CreateDirectory(salesTotalDir);
+Directory.CreateDirectory(storesDirectory);
+var salesFiles = FindFiles(storesDirectory);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
